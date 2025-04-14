810b08764c69e2699cedba4dbefc9d19e2999a1e
Xwilarg <xwilarg@protonmail.com>
2025-04-14 20:06:33 +0200

    Add level requirement debug

--- Assets/Scripts/Prop/LevelRequirement.cs
+++ Assets/Scripts/Prop/LevelRequirement.cs
@@ -7,5 +7,22 @@ namespace PreggoJam.Prop
         [SerializeField]
         private int _targetRequirement;
         public int TargetRequirement => _targetRequirement;
+
+        private Color[] Colors = new Color[]
+        {
+            Color.green,
+            Color.blue,
+            Color.yellow,
+            Color.red,
+            Color.black
+        };
+        private void OnDrawGizmos()
+        {
+            if (_targetRequirement > 0)
+            {
+                Gizmos.color = Colors[_targetRequirement - 1];
+                Gizmos.DrawCube(transform.position, Vector2.one * .5f);
+            }
+        }
     }
 }
